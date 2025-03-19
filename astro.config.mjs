7c10b3dfa2b170220c238e5dfa799e7b392730d2
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-19 10:50:17 +0100

    update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jpiedra181.github.io',
-  base: 'list-tasks',
+  site: 'https://jpiedra181.github.io/',
+  base: '/list-tasks/',
   vite: {
     plugins: [tailwindcss()]
   }
