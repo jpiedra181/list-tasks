6b051afbbb2a1e21c6f28f1703738030155beed5
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-19 09:30:23 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://jpiedra181.github.io',
+  base: 'list-tasks',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
